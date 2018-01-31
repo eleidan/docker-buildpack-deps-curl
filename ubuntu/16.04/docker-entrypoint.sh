42ad4a93a02208bae726a0a78b3173b7804e8f74
Oleg Kulyk <helh.saintman@gmail.com>
2018-01-31 16:43:36 +0200

    Ensure phantom user owns his home directory

--- ubuntu/16.04/docker-entrypoint.sh
+++ ubuntu/16.04/docker-entrypoint.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 THE_USER=phantom
-OLD_UID=$(id -u $THE_USER)
-THE_UID=$OLD_UID
+OLD_UID=$(id -u ${THE_USER})
+THE_UID=${OLD_UID}
 
 ## 1. Fetch UID and GID from /home/phantom/app
-APP_DIR=/home/$THE_USER/app
-if [ -d $APP_DIR ]; then
-  THE_UID=$(stat --format '%u' $APP_DIR)
-  THE_GID=$(stat --format '%g' $APP_DIR)
+APP_DIR=/home/${THE_USER}/app
+if [ -d ${APP_DIR} ]; then
+  THE_UID=$(stat --format '%u' ${APP_DIR})
+  THE_GID=$(stat --format '%g' ${APP_DIR})
 fi
 
 ## 2. Update UID and GID of the user
-if [[ $OLD_UID != $THE_UID && $THE_UID != "0" ]]; then
-  usermod -u $THE_UID $THE_USER \
-  && groupmod -g $THE_GID $THE_USER \
-  && echo "Updated UID and GID for the '$THE_USER' user according to the '$APP_DIR' directory."
+if [[ ${OLD_UID} != ${THE_UID} && ${THE_UID} != "0" ]]; then
+  usermod -u ${THE_UID} ${THE_USER} \
+  && groupmod -g ${THE_GID} ${THE_USER} \
+  && chown ${THE_UID}:${THE_GID} ${HOME} \
+  && echo "Updated UID and GID for the '${THE_USER}' user according to the '${APP_DIR}' directory."
 fi
 
-exec gosu $THE_USER "$@"
+exec gosu ${THE_USER} "$@"
